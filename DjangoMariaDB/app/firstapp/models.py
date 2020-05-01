be8b07475d8673f0468bc80edcfea5e27410aaaf
MarioChico <36939569+MarioChico@users.noreply.github.com>
2020-04-30 23:29:32 -0500

    T3
    Tarea 3

--- DjangoMariaDB/app/firstapp/models.py
+++ DjangoMariaDB/app/firstapp/models.py
@@ -36,14 +36,14 @@ class Movie(models.Model):
         db_table = 'movie'
 
 
-#from rest_framework import serializers
+from rest_framework import serializers
 
-#class PaymentSerializer(serializers.Serializer):
-    #amount = serializers.IntegerField(required=True, min_value=0)
-    #name = serializers.CharField(required=True, max_length=128)
-    #type = serializers.IntegerField(required=True, min_value=0)
+class PaymentSerializer(serializers.Serializer):
+    amount = serializers.IntegerField(required=True, min_value=0)
+    name = serializers.CharField(required=True, max_length=128)
+    type = serializers.IntegerField(required=True, min_value=0)
 
-#class ValidateFormSerializer(serializers.Serializer):
-    #var1 = serializers.CharField(required=True, max_length=250)
-    #var2 = serializers.CharField(required=True, max_length=250)
+class ValidateFormSerializer(serializers.Serializer):
+    var1 = serializers.CharField(required=True, max_length=250)
+    var2 = serializers.CharField(required=True, max_length=250)
     #payments = serializers.ListField(child=PaymentSerializer)
--- DjangoMariaDB/app/firstapp/urls.py
+++ DjangoMariaDB/app/firstapp/urls.py
@@ -3,7 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('client/login',views.login,name='login'),
+    #path('', views.paito, name='paito'),
+    # ex: /1.0/5/
+    #path('states/<int:id>/', views.state, name='state'),
+    #path('states/', views.states, name='states'),
+    #path('movie/get/', views.movies, name='movies'),
+    #path('moviespost/post/', views.movies, name='movies'),
     path('generate_password/<str:password>/',views.makepassword,name='makepassword'),
     path('client/list/', views.showMovies, name = 'showMovies'),
+    path('', views.vista, name = 'vista'),
+    path('',views.vista,name='vista'),
+    path('dos.html',views.vista2,name='vista2'),
+    #path('client/update/', views.updateclient, name='updateclient'),
+    #path('client/get/', views.getclient, name='getclient'),
+    #path('client/delete/', views.deleteclient, name='deleteclient'),
+
 ]
